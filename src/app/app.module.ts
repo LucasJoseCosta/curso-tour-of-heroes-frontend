b3e5148cc140e2a0d4d289367188a10a1178aa4a
LucasJoseCosta <lucjoc.1@hotmail.com>
2023-02-20 17:09:49 -0300

    Ajustado compentes com material e criado um module para imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { MaterialModule } from './material/material.module';
 import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
@@ -30,12 +25,7 @@ import { MessagesComponent } from './messages/messages.component';
     FormsModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatButtonModule,
-    MatExpansionModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatTooltipModule,
-    MatCardModule,
+    MaterialModule,
     AppRoutingModule,
   ],
   providers: [],
